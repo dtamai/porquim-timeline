c87f5ac5a2544769579d9f35176508a106521160
Daniel Tamai <daniel.tamai@gmail.com>
2018-02-12 15:25:51 -0200

    View entries for a single snapshot

--- PorquimTimeline.elm
+++ PorquimTimeline.elm
@@ -6,7 +6,7 @@ import Html exposing (..)
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
-        { model = {}
+        { model = initialModel
         , view = view
         , update = update
         }
@@ -16,8 +16,37 @@ main =
 -- MODEL
 
 
+type alias Snapshot =
+    { referenceDate : String
+    , entries : List SnapshotEntry
+    }
+
+
+type alias SnapshotEntry =
+    { bucketName : String
+    , value : Float
+    }
+
+
 type alias Model =
-    {}
+    { currentSnapshot : Snapshot }
+
+
+initialModel : Model
+initialModel =
+    Model
+        (Snapshot "Jan/18"
+            [ SnapshotEntry "Bucket A" 1234.56
+            , SnapshotEntry "Bucket B" 2345.67
+            ]
+        )
+
+
+snapshotTotal : Snapshot -> Float
+snapshotTotal snapshot =
+    snapshot.entries
+        |> List.map .value
+        |> List.sum
 
 
 
@@ -39,4 +68,35 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Porquim Timeline" ]
+    div []
+        [ viewSnapshot model.currentSnapshot
+        ]
+
+
+viewSnapshot : Snapshot -> Html Msg
+viewSnapshot snapshot =
+    section []
+        [ div [] [ text snapshot.referenceDate ]
+        , viewSnapshotEntries snapshot.entries
+        , viewSnapshotTotal (snapshotTotal snapshot)
+        ]
+
+
+viewSnapshotEntries : List SnapshotEntry -> Html Msg
+viewSnapshotEntries entries =
+    ul [] (List.map viewSnapshotEntryItem entries)
+
+
+viewSnapshotEntryItem : SnapshotEntry -> Html Msg
+viewSnapshotEntryItem entry =
+    li []
+        [ span [] [ text entry.bucketName ]
+        , span [] [ text "  -  " ]
+        , span [] [ text (toString entry.value) ]
+        ]
+
+
+viewSnapshotTotal : Float -> Html Msg
+viewSnapshotTotal total =
+    div []
+        [ text ("Total  -  " ++ toString total) ]
