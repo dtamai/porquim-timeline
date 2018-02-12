90d61988e561f06f85b5435a7cb7771194c9b6ce
Daniel Tamai <daniel.tamai@gmail.com>
2018-02-12 20:10:34 -0200

    Add control for navigation on timeline

--- PorquimTimeline.elm
+++ PorquimTimeline.elm
@@ -1,6 +1,8 @@
 module PorquimTimeline exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 main : Program Never Model Msg
@@ -29,17 +31,32 @@ type alias SnapshotEntry =
 
 
 type alias Model =
-    { currentSnapshot : Snapshot }
+    { prevSnapshots : List Snapshot
+    , currentSnapshot : Snapshot
+    , nextSnapshots : List Snapshot
+    }
 
 
 initialModel : Model
 initialModel =
     Model
+        [ (Snapshot "Dez/17"
+            [ SnapshotEntry "Bucket A" 1123.45
+            , SnapshotEntry "Bucket B" 2234.56
+            ]
+          )
+        ]
         (Snapshot "Jan/18"
             [ SnapshotEntry "Bucket A" 1234.56
             , SnapshotEntry "Bucket B" 2345.67
             ]
         )
+        [ (Snapshot "Fev/18"
+            [ SnapshotEntry "Bucket A" 1345.67
+            , SnapshotEntry "Bucket B" 2456.78
+            ]
+          )
+        ]
 
 
 snapshotTotal : Snapshot -> Float
@@ -49,17 +66,51 @@ snapshotTotal snapshot =
         |> List.sum
 
 
+prevSnapshot : Model -> Model
+prevSnapshot model =
+    case List.head model.prevSnapshots of
+        Just snapshot ->
+            { model
+                | prevSnapshots = List.drop 1 model.prevSnapshots
+                , currentSnapshot = snapshot
+                , nextSnapshots = model.currentSnapshot :: model.nextSnapshots
+            }
+
+        Nothing ->
+            model
+
+
+nextSnapshot : Model -> Model
+nextSnapshot model =
+    case List.head model.nextSnapshots of
+        Just snapshot ->
+            { model
+                | prevSnapshots = model.currentSnapshot :: model.prevSnapshots
+                , currentSnapshot = snapshot
+                , nextSnapshots = List.drop 1 model.nextSnapshots
+            }
+
+        Nothing ->
+            model
+
+
 
 -- UPDATE
 
 
 type Msg
-    = None
+    = PrevSnapshot
+    | NextSnapshot
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        PrevSnapshot ->
+            prevSnapshot model
+
+        NextSnapshot ->
+            nextSnapshot model
 
 
 
@@ -70,6 +121,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ viewSnapshot model.currentSnapshot
+        , viewControls
         ]
 
 
@@ -100,3 +152,11 @@ viewSnapshotTotal : Float -> Html Msg
 viewSnapshotTotal total =
     div []
         [ text ("Total  -  " ++ toString total) ]
+
+
+viewControls : Html Msg
+viewControls =
+    div []
+        [ a [ href "#", onClick PrevSnapshot ] [ text "«" ]
+        , a [ href "#", onClick NextSnapshot ] [ text "»" ]
+        ]
